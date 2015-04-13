ea75954eea91f2e76344a807bceb6af7e984c863
Boris Rorsvort <boris.rorsvort@gmail.com>
2015-04-13 11:56:03 +0200

    added locale info on main page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+  $('span.sendmail').each(function() {
+    var $span = $(this),
+        data = $(this).text().split('+');
+    data[3] = data[3] || data[0] + '@' + data[1] + '.' + data[2];
+    $span.after('<a href="mailto:' + data[0] + '@' + data[1] + '.' + data[2] + '">' + data[3] + '</a>').remove();
+  });
+});
